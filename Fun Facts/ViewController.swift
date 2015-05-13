087dd783102059831ff65c0b4414fc70f56d4f15
robboyland <boylandrobert@gmail.com>
2015-05-13 14:57:14 +0100

    Initial Commit

---
+++ Fun Facts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fun Facts
+//
+//  Created by robb on 13/05/2015.
+//  Copyright (c) 2015 robb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
